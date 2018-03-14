8b3106771a1011fc13515833753eabab0805a604
Nicolas Buduroi <nbuduroi@gmail.com>
2018-03-14 19:16:24 -0400

    Replace custom color theme by zenburn with some overrides

--- .emacs.d/site-lisp/budu-style.el
+++ .emacs.d/site-lisp/budu-style.el
@@ -1,6 +1,6 @@
 ;;; default fonts for graphical mode
 (let (font (fonts (list "Bitstream Vera Sans Mono-12"
-                    "Lucida Console-12")))
+                        "Lucida Console-12")))
   (while fonts
     (condition-case nil
       (progn
@@ -29,64 +29,12 @@
                         (frame-char-height)))))))
 
 (set-frame-size-according-to-resolution)
-
 (set-frame-position (selected-frame)  100 20)
 
-(require 'color-theme)
-(defun color-theme-budu-midnight ()
-  (interactive)
-  (color-theme-install
-    '(color-theme-midnight
-       ((font . "fixed")
-         (width . 130)
-         (height . 50)
-         (background-color . "black")
-         (foreground-color . "white")
-         (background-mode . dark)
-         (mouse-color . "grey85")
-         (cursor-color . "grey85"))
-       (default ((t (nil))))
-       (font-lock-builtin-face
-         ((t (:foreground "SteelBlue"))))
-       (font-lock-comment-face
-         ((t (:italic t :foreground "orange3" :slant oblique))))
-       (font-lock-constant-face
-         ((t (:bold t :foreground "Gold" :weight bold))))
-       (font-lock-doc-face
-         ((t (:italic t :slant oblique :foreground "BurlyWood"))))
-       (font-lock-doc-string-face
-         ((t (:italic t :slant oblique :foreground "BurlyWood"))))
-       (font-lock-function-name-face
-         ((t (:bold t :foreground "LightSkyBlue" :weight bold))))
-       (font-lock-keyword-face
-         ((t (:foreground "LightGreen"))))
-       (font-lock-preprocessor-face
-         ((t (:bold t :foreground "Gold" :weight bold))))
-       (font-lock-reference-face
-         ((t (:foreground "SteelBlue"))))
-       (font-lock-string-face
-         ((t (:italic t :foreground "BurlyWood" :slant oblique))))
-       (font-lock-type-face
-         ((t (:bold t :foreground "PaleGreen" :weight bold))))
-       (font-lock-variable-name-face
-         ((t (:foreground "Aquamarine"))))
-       (font-lock-warning-face
-         ((t (:bold t :foreground "chocolate" :weight bold))))
-       (highline-face
-         ((t (:background "grey12"))))
-       (setnu-line-number-face
-         ((t (:background "Grey15" :foreground "White" :bold t))))
-       (show-paren-match-face
-         ((t (:background "grey30"))))
-       (region
-         ((t (:background "grey15"))))
-       (highlight
-         ((t (:background "blue"))))
-       (secondary-selection
-         ((t (:background "navy"))))
-       (widget-field-face
-         ((t (:background "navy"))))
-       (widget-single-line-field-face
-         ((t (:background "royalblue")))))))
-
-(color-theme-budu-midnight)
+(defvar zenburn-override-colors-alist
+  '(("zenburn-bg" . "#181818")
+    ("zenburn-bg+05" . "#282828")
+    ("zenburn-bg+1"  . "#2F2F2F")
+    ("zenburn-bg+2"  . "#3F3F3F")
+    ("zenburn-bg+3"  . "#4F4F4F")))
+(load-theme 'zenburn t)
