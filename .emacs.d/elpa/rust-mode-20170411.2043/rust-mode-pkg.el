b9559f86ba3baa599b75a05ce3daf57f1bbf4e3f
Nicolas Buduroi <nbuduroi@gmail.com>
2018-03-14 19:12:11 -0400

    Upgrade packages, add some more and clean up

---
+++ .emacs.d/elpa/rust-mode-20170411.2043/rust-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rust-mode" "20170411.2043" "A major emacs mode for editing Rust source code" '((emacs "24.0")) :commit "dae5af71ebf4b5c6797ef057e8a0ebf655bcdbfb" :url "https://github.com/rust-lang/rust-mode" :keywords '("languages"))
