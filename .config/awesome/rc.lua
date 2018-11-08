aa002332a83c8eafb24d75f3ea11d6937869a0c5
Nicolas Buduroi <nbuduroi@gmail.com>
2018-11-08 12:33:01 -0500

    [awesome] Change default terminal to termite

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -41,7 +41,7 @@ end
 beautiful.init("/home/budu/.config/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "urxvt"
+terminal = "termite"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
